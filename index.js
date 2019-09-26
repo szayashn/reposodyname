a5a423611f0f856494f43c0e7d8aaa6726682fbe
unknown <szayashn@gmail.com>
2019-09-26 13:50:19 -0700

    make ninjas running

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
+app.use(express.static(__dirname + "/static"));
 //create list of users
 let list_of_users = []
 
